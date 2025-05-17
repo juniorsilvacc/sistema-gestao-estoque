5423b4aca5a116e2a53f59c5e41656a5172237f6
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-05-17 00:41:04 -0300

    chore: adjusting admin class name

--- categories/admin.py
+++ categories/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from . import models
 
 
-class CategoyAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description',)
     search_fields = ('name',)
 
-admin.site.register(models.Category, CategoyAdmin)
+admin.site.register(models.Category, CategoryAdmin)
