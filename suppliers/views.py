b96ea7962f01fcc2a14c646aca9f364d76f265f7
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-05-17 01:09:24 -0300

    feat: CRUD suppliers

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from . import models
+from . import forms
 
-# Create your views here.
+
+class SupplierListView(ListView):
+    model = models.Supplier
+    template_name = 'supplier_list.html'
+    context_object_name = 'suppliers'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        
+        return queryset.order_by('id')
+
+class SupplierCreateView(CreateView):
+    model = models.Supplier
+    template_name = 'supplier_create.html'
+    form_class = forms.SupplierForm
+    success_url = reverse_lazy('supplier_list')
+
+class SupplierDetailView(DetailView):
+    model = models.Supplier
+    template_name = 'supplier_detail.html'
+
+class SupplierUpdateView(UpdateView):
+    model = models.Supplier
+    template_name = 'supplier_update.html'
+    form_class = forms.SupplierForm
+    success_url = reverse_lazy('supplier_list')
+
+class SupplierDeleteView(DeleteView):
+    model = models.Supplier
+    success_url = reverse_lazy('supplier_list')
