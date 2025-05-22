973b1cbf8487c01d57606662e07bda52fd940f1c
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-05-22 01:51:08 -0300

    feat: adding admin

--- outflows/admin.py
+++ outflows/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class OutflowAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'description',)
+    search_fields = ('product__title',)
+
+admin.site.register(models.Outflow, OutflowAdmin)
