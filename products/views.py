1c10dec07a2bc3e75bf5b0a7754ba7273a9d4d5e
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-05-22 03:23:33 -0300

    feat: CRUD products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from . import models
+from . import forms
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = models.Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.GET.get('title')
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        
+        return queryset.order_by('id')
+
+class ProductCreateView(CreateView):
+    model = models.Product
+    template_name = 'product_create.html'
+    form_class = forms.ProductForm
+    success_url = reverse_lazy('product_list')
+
+class ProductDetailView(DetailView):
+    model = models.Product
+    template_name = 'product_detail.html'
+
+class ProductUpdateView(UpdateView):
+    model = models.Product
+    template_name = 'product_update.html'
+    form_class = forms.ProductForm
+    success_url = reverse_lazy('product_list')
+
+class ProductDeleteView(DeleteView):
+    model = models.Product
+    success_url = reverse_lazy('product_list')
