81654b9cf5884b5238684b2667110397ec90135c
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-05-22 19:38:16 -0300

    feat: creating the home page and 500 and 404 error components

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 from app.views import custom_500
 from app.views import custom_404
-
+from .views import force_500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', views.home, name='home'),
     
     path('', include('brands.urls')),
     path('', include('categories.urls')),
--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 
-def custom_500(request):
-    return render(request, 'pages/500.html', status=500)
+def force_500(request):
+    1 / 0  # Isso vai gerar ZeroDivisionError e causar erro 500
+    return HttpResponse("Isso nunca vai ser executado")
+
+def home(request):
+    context = {}
+    return render(request, 'pages/home.html')
+
+def custom_500(request, exception=None):
+    return render(request, 'errors/_500.html', status=500)
 
 def custom_404(request, exception=None):
-    return render(request, 'pages/404.html', status=404)
+    return render(request, 'errors/_404.html', status=404)
