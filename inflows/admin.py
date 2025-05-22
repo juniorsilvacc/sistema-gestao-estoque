364fd486d2b390223cd2c71ccb51e55587782393
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-05-22 01:49:36 -0300

    feat: CRUD inflows

--- inflows/admin.py
+++ inflows/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class InflowAdmin(admin.ModelAdmin):
+    list_display = ('supplier', 'product', 'quantity', 'description',)
+    search_fields = ('supplier__name', 'product__title')
+
+admin.site.register(models.Inflow, InflowAdmin)
