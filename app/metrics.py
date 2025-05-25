6b26af21361859059075959a08173b9622d1756e
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-05-25 01:10:43 -0300

    feat: grafics in dashboard

--- app/metrics.py
+++ app/metrics.py
@@ -2,6 +2,8 @@ from products.models import Product
 from outflows.models import Outflow
 from django.utils.formats import number_format
 from django.db.models import Sum
+from datetime import timedelta
+from django.utils import timezone
 
 
 def get_product_metrics():
@@ -39,3 +41,77 @@ def get_sales_metrics():
     }
 
     return sales_metrics
+
+
+def get_last_7_days():
+    today = timezone.now().date()
+    return [today - timedelta(days=i) for i in range(6, -1, -1)]
+
+def get_daily_sales_data():
+    """
+    Total monetário diário de saídas (considerando preço de venda do produto * quantidade).
+    """
+    from products.models import Product
+
+    dates = get_last_7_days()
+    labels = []
+    values = []
+
+    for day in dates:
+        outflows = Outflow.objects.filter(created_at__date=day)
+        total = 0
+
+        for outflow in outflows.select_related('product'):
+            total += outflow.quantity * outflow.product.selling_price
+
+        labels.append(day.strftime('%d/%m'))
+        values.append(float(total))
+
+    return {
+        'dates': labels,
+        'values': values
+    }
+
+def get_daily_sales_quantity_data():
+    """
+    Quantidade total de produtos que saíram por dia.
+    """
+    dates = get_last_7_days()
+    labels = []
+    quantities = []
+
+    for day in dates:
+        quantity = Outflow.objects.filter(created_at__date=day).aggregate(Sum('quantity'))['quantity__sum'] or 0
+        labels.append(day.strftime('%d/%m'))
+        quantities.append(quantity)
+
+    return {
+        'dates': labels,
+        'values': quantities
+    }
+
+
+def get_top_10_stock_products():
+    """
+    Estoque Atual por Produto (Top 10)
+    """
+    top_products = Product.objects.order_by('-quantity')[:10]
+    return {
+        'labels': [p.title for p in top_products],
+        'data': [p.quantity for p in top_products],
+    }
+
+
+def get_top_5_sold_products():
+    """
+    Top 5 Produtos Mais Vendidos
+    """
+    top_sold = (
+        Outflow.objects.values('product__title')
+        .annotate(total_sold=Sum('quantity'))
+        .order_by('-total_sold')[:5]
+    )
+    return {
+        'labels': [item['product__title'] for item in top_sold],
+        'data': [item['total_sold'] for item in top_sold],
+    }
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from . import metrics
 
@@ -11,10 +12,18 @@ def custom_404(request, exception=None):
 def home(request):
     product_metrics = metrics.get_product_metrics()
     sales_metrics = metrics.get_sales_metrics()
+    daily_sales_data = metrics.get_daily_sales_data()
+    daily_sales_quantity_data = metrics.get_daily_sales_quantity_data()
+    top_10_stock = metrics.get_top_10_stock_products()
+    top_5_sold = metrics.get_top_5_sold_products()
 
     context = {
         'product_metrics': product_metrics,
-        'sales_metrics': sales_metrics
+        'sales_metrics': sales_metrics,
+        'daily_sales_data': json.dumps(daily_sales_data),
+        'daily_sales_quantity_data': json.dumps(daily_sales_quantity_data),
+        'top_10_stock': json.dumps(top_10_stock),
+        'top_5_sold': json.dumps(top_5_sold)
     }
 
     return render(request, 'pages/home.html', context)
