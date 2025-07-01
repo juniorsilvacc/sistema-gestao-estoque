7fff60fdba2d25d8ee5d30c4baa483019362c603
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-07-01 02:58:54 -0300

    feat: signals to notify when an exit is made

--- outflows/signals.py
+++ outflows/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from outflows.models import Outflow
+from services.notify import Notify
+from datetime import datetime
 
 
 @receiver(post_save, sender=Outflow)
@@ -10,3 +12,19 @@ def update_product_quantity(sender, instance, created, **kwargs):
             product = instance.product
             product.quantity -= instance.quantity
             product.save()
+
+
+@receiver(post_save, sender=Outflow)
+def send_outflow_event(sender, instance, **kwargs):
+    notify = Notify()
+
+    data = {
+        'event_type': 'outflow',
+        'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'product': instance.product.title,
+        'product_selling_price': float(instance.product.selling_price),
+        'quantity': instance.quantity,
+        'description': instance.description,
+    }
+
+    notify.send_order_event(data)
