4166ed9f62dead8b8bb783a282058f1f12f9d125
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-07-01 02:58:17 -0300

    feat: integrating the notification system

---
+++ services/notify.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class Notify:
+
+    def __init__(self):
+        self.__base_url = 'http://host.docker.internal:8001'
+
+    def send_order_event(self, data):
+        requests.post(
+            url=f'{self.__base_url}/api/v1/webhooks/order/',
+            json=data,
+        )
+        
