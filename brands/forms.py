4b9f171e24789bdb109bbcd89ee030ebb144ae2a
juniorsilvaeng <juniorsilvacc@hotmail.com>
2025-07-24 19:32:53 -0300

    feat: changes to brand input fields and views

--- brands/forms.py
+++ brands/forms.py
@@ -6,12 +6,14 @@ class BrandForm(forms.ModelForm):
     
     class Meta:
         model = models.Brand
-        fields = ['name', 'description']
+        fields = ['name', 'website', 'description']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'website': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3})
         }
         labels = {
             'name': 'Nome',
+            'website': 'Website',
             'description': 'Descrição'
         }
--- brands/models.py
+++ brands/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Brand(models.Model):
     name = models.CharField(max_length=500)
+    website = models.URLField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
