b2b19e177efe781d693dd080076a57ece4dee248
juniorsilvacc <juniorsilvacc@hotmail.com>
2025-07-26 16:32:03 -0300

    fix: fix in logout route

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('', views.home, name='home'),
     
